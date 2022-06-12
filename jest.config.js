d6fa302bdfa854a6c1ed9a2f9438bfb0b2ce8664
Caio Arruda <47371713+arrudacaio@users.noreply.github.com>
2022-06-12 13:19:46 -0300

    chore: finish ci cd

--- jest.config.js
+++ jest.config.js
@@ -126,7 +126,6 @@ module.exports = {
   roots: [
     '<rootDir>/src',
     '<rootDir>/test',
-    '<rootDir>/lambdas',
   ],
 
   // Allows you to use a custom runner instead of Jest's default test runner
