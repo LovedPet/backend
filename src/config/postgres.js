18bcab8ff05772d22a93c95c9df3b51ab2d91f97
Caio Arruda <47371713+arrudacaio@users.noreply.github.com>
2022-06-19 20:00:55 -0300

    chore: adds postgres config

---
+++ src/config/postgres.js
@@ -0,0 +1,32 @@
+const {
+  DATABASE_HOST,
+  DATABASE_PORT,
+  DATABASE_USER,
+  DATABASE_PASSWORD,
+  DATABASE_NAME,
+} = process.env
+
+const defaultSettings = {
+  dialect: 'postgres',
+  host: DATABASE_HOST,
+  port: DATABASE_PORT,
+  username: DATABASE_USER,
+  password: DATABASE_PASSWORD,
+  database: DATABASE_NAME,
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+  pool: {
+    max: Number(DATABASE_MAX_CONNECTIONS) || 50,
+    min: 1,
+  },
+}
+
+module.exports = {
+  development: defaultSettings,
+  test: defaultSettings,
+  production: defaultSettings,
+}
